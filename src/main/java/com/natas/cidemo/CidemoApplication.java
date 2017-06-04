929e86e600e3318ec71b38e96a85b3a81c5857c4
NatasG <gaoshuai1205@gmail.com>
2017-06-04 14:22:11 +0800

    init

---
+++ src/main/java/com/natas/cidemo/CidemoApplication.java
@@ -0,0 +1,12 @@
+package com.natas.cidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidemoApplication.class, args);
+	}
+}
